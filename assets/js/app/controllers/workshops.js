1cfd773ea5a97b734fdb00926822d27a76ad982a
Armin Pfurtscheller <armin.pfurtscheller@googlemail.com>
2017-09-03 23:21:14 +0200

    Update 2017-09-03T21:20:41.767Z

--- assets/js/app/controllers/workshops.js
+++
@@ -1,8 +0,0 @@
-/*!
- * eFit Website
- * An app for financial training in educational environments
- * http://www.e-fit.com
- * @author Armin Pfurtscheller
- * @version 1.0.0
- * Copyright 2017. MIT licensed.
- */
